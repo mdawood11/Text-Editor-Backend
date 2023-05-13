601bd6d417d9819cffbe0342c23b71f993e355b5
mohamad ghaznawy <mdawoodghaznawy@yahoo.com>
2023-05-12 19:19:37 -0700

    Added configure workbox plugins for a service worker and manifest file. in webpack.config.js file

--- client/src-sw.js
+++ client/src-sw.js
@@ -30,7 +30,6 @@ registerRoute(({ request }) => request.mode === "navigate", pageCache);
 registerRoute(
   ({ request }) => ["style", "script", "worker"].includes(request.destination),
   new StaleWhileRevalidate({
-    // Name of the cache storage.
     cacheName: "asset-cache",
     plugins: [
       new CacheableResponsePlugin({
